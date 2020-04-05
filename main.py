1ecfc3f96413c21ba2eb99f502a5207ad73a3299
3-24 <dev.youngseok@gmail.com>
2020-04-05 23:20:17 +0900

    Splitted codes and create data dir if not exist

--- main.py
+++ main.py
@@ -1,133 +1,14 @@
 #/usr/bin/env/python3
-import discord, os, sys,sqlite3, count, time
+import discord, os, sys, count, time
 from datetime import datetime, timedelta
+from attendance import *
 
 os.chdir(sys.path[0]) # changed working dir as script dir to get fixed relative access to data
 TOKEN = os.environ["nalgang_TOKEN"]
 client = discord.Client()
 
-class Member:
-    conn = sqlite3.connect("./data/member.db")
-    c = conn.cursor()
-    def __init__(self, id_num, name="temp"):
-        self.id_num = id_num
-        self.name = name
-        return
-
-    def add_db(self,point=0,combo=0):
-        Member.c.execute('''INSERT INTO Members VALUES (:Id, :point, :combo)''',{"Id":self.id_num, "point":point, "combo":combo})
-    
-    def get_point(self):
-        Member.c.execute('''SELECT point FROM Members WHERE id=:Id''', {"Id":self.id_num})
-        P = Member.c.fetchall()
-        if not P:
-            self.add_db()
-            return 0
-        else:
-            assert len(P) == 1
-            return P[0][0]
-
-
-    def add_point(self,p):
-        prev_point = self.get_point()
-        Member.c.execute('''UPDATE Members SET point=:point WHERE id=:Id''', {"Id":self.id_num, "point":p+prev_point})
-        Member.conn.commit()
-        return
-
-    def get_combo(self):
-        Member.c.execute('''SELECT combo FROM Members WHERE id=:Id''', {"Id":self.id_num})
-        P = Member.c.fetchall()
-        if not P:
-            self.add_db()
-            return 0
-        else:
-            assert len(P) == 1
-            return P[0][0]
-
-    def update_combo(self,reset=False):
-        if reset:
-            Member.c.execute('''UPDATE Members SET combo=:combo WHERE id=:Id''', {"Id":self.id_num, "combo":0})
-            Member.conn.commit()
-            return
-        else:
-            combo = self.get_combo()
-            Member.c.execute('''UPDATE Members SET combo=:combo WHERE id=:Id''', {"Id":self.id_num, "combo":combo+1})
-            Member.conn.commit()
-            return
-            
-
-    def check_attendance(self):
-        Member.c.execute('''SELECT * FROM AttendanceTable WHERE id= :Id''', {"Id": self.id_num})
-        attendance = Member.c.fetchall()
-        return attendance != []
-    
-    def set_attendance(self):
-        Member.c.execute('''INSERT INTO AttendanceTable VALUES (:Id)''',{"Id":self.id_num})
-        Member.conn.commit()
-        return
-
-    def update_attendance_and_point(self): # returb added point, combo point
-        if self.check_attendance(): return -1, 0
-        self.set_attendance()
-        pointmap = [20,10,3,1]
-        attcount = min(count.read(),3)
-        p = pointmap[attcount]
-        self.add_point(p)
-        cp=self.get_combo()
-        # cp modify
-        cp = min(cp,7)
-        self.update_combo()
-        self.add_point(cp)
-        count.add()
-        return p, cp
-
-
-    def give_point(self,member,point):
-        self.add_point(-point)
-        member.add_point(point)
-        return
-
-
-def sql_access(func):
-    def access_table_wrapper(*args, **kwargs):
-        kwargs['conn'] = sqlite3.connect("./data/member.db")
-        kwargs['c'] = kwargs['conn'].cursor()
-        r = func(*args, **kwargs)
-        kwargs['conn'].close()
-        return r
-    return access_table_wrapper
-
-@sql_access
-def table_init(c,conn):
-    c.execute('''CREATE TABLE IF NOT EXISTS Members (id integer, point integer, combo integer)''')
-    c.execute('''CREATE TABLE IF NOT EXISTS AttendanceTable (id integer)''')
-    conn.commit()
-    c.close()
-    return
-
-
-@sql_access
-def combo_reset(c,conn):
-    c.execute('''SELECT id FROM AttendanceTable''')
-    attendenceList=[i[0] for i in c.fetchall()]
-    c.execute('''SELECT id FROM Members''')
-    memberList=[i[0] for i in c.fetchall()]
-    for member in memberList:
-        if member not in attendenceList:
-            m=Member(member)
-            m.update_combo(True)
-    c.close()
-    return
-
-
-@sql_access
-def day_reset(c,conn):
-    combo_reset()
-    c.execute('''DROP TABLE AttendanceTable''')
-    conn.commit()
-    table_init()
-    count.save(0)
-    return
+if not os.path.exists("data"):
+    os.makedirs("data")
 
 
 def check_int(s):
