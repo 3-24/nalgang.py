8c1920760368611ade64e23c9b706c198e18a02a
3-24 <dev.youngseok@gmail.com>
2020-04-07 19:45:28 +0900

    minor improvements

--- attendance.py
+++ attendance.py
@@ -1,10 +1,8 @@
 import sqlite3
-import count
+from access_data import *
 
 conn = sqlite3.connect("./data/member.db")
 c = conn.cursor()
-
-table_init()
 
 class Member:
     def __init__(self, id_num, name="temp"):
@@ -29,7 +27,6 @@ class Member:
     def add_point(self,p):
         prev_point = self.get_point()
         c.execute('''UPDATE Members SET point=:point WHERE id=:Id''', {"Id":self.id_num, "point":p+prev_point})
-        conn.commit()
         return
 
     def get_combo(self):
@@ -48,7 +45,6 @@ class Member:
         else:
             combo = self.get_combo()
             c.execute('''UPDATE Members SET combo=:combo WHERE id=:Id''', {"Id":self.id_num, "combo":combo+1})
-        conn.commit()
         return
             
 
@@ -59,14 +55,13 @@ class Member:
     
     def set_attendance(self):
         c.execute('''INSERT INTO AttendanceTable VALUES (:Id)''',{"Id":self.id_num})
-        conn.commit()
         return
 
     def update_attendance_and_point(self): # returb added point, combo point
         if self.check_attendance(): return -1, 0
         self.set_attendance()
         pointmap = [20,10,3,1]
-        attcount = min(count.read(),3)
+        attcount = min(count_read(),3)
         p = pointmap[attcount]
         self.add_point(p)
         cp=self.get_combo()
@@ -74,11 +69,12 @@ class Member:
         cp = min(cp,7)
         self.update_combo()
         self.add_point(cp)
-        count.add()
+        count_add()
         return p, cp
 
 
     def give_point(self,member,point):
+        assert self.get_point() >= point >= 0
         self.add_point(-point)
         member.add_point(point)
         return
@@ -87,7 +83,6 @@ class Member:
 def table_init():
     c.execute('''CREATE TABLE IF NOT EXISTS Members (id integer, point integer, combo integer)''')
     c.execute('''CREATE TABLE IF NOT EXISTS AttendanceTable (id integer)''')
-    conn.commit()
     return
 
 
@@ -100,14 +95,12 @@ def combo_reset():
         if member not in attendenceList:
             m=Member(member)
             m.update_combo(True)
-    c.close()
     return
 
 
 def day_reset():
     combo_reset()
     c.execute('''DROP TABLE AttendanceTable''')
-    conn.commit()
     table_init()
-    count.save(0)
+    count_save(0)
     return
