3876f159899e95aafcbcc7ce71bd1165d48c3e1e
3-24 <dev.youngseok@gmail.com>
2020-04-28 23:19:14 +0900

    Remove emojify_number in attendance

--- attendance.py
+++ attendance.py
@@ -124,10 +124,6 @@ def get_all_attendance_info():
     c.execute('''SELECT * FROM AttendanceTable''')
     return c.fetchall()
 
-def emojify_number(s):
-    d = {'0': ':zero:', '1': ':one:', '2': ':two:', '3': ':three:', '4': ':four:', '5': ':five:', '6': ':six:', '7': ':seven:', '8': ':eight:', '9': ':nine:'}
-    return ''.join( (d[s[i]] for i in range(len(s))) )
-
 def scoreboard(guild):
     c.execute('''SELECT id, point FROM Members ORDER BY point DESC''')
     s=''
