d5873911ced72cb355f61a9732382407448a87d1
3-24 <dev.youngseok@gmail.com>
2020-08-17 19:21:27 +0900

    Add 날갱관리자 role

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-admin_ids = [419370093273939981] # put discord ids in list
 point_by_rank = [10, 5, 3, 1] # 1st get 10 points, 2st get 5 points, 3st get 3 points, others get 1 points.
 week_bonus = 20
 month_bonus = 100
