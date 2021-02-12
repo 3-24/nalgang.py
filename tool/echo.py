089ec0f744bb4dfbd9105b8b54539d1066f095d9
Youngseok Choe <dev.youngseok@gmail.com>
2021-02-12 14:07:42 +0900

    Create echo bot for test tool

---
+++ tool/echo.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import discord
+import random
+import os
+
+TOKEN = os.environ["echo_TOKEN"]
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Echo bot is ready')
+
+@client.event
+async def on_message(message):
+    if message.author.bot:
+        return
+
+    await message.channel.send(message.content)
+
+client.run(TOKEN)
